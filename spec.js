4c4449a54837749d20128dd0f5eb59087e3fb202
armstrongsam25 <armstrongsam25@gmail.com>
2019-02-23 23:28:41 -0500

    Updated spec.js
    updated a couple of css element tags to work

--- spec.js
+++ spec.js
@@ -8,7 +8,7 @@ describe('Instagram fast commenter', function() {
   var numberOfPostsOnTargetProfile = 69;
   const commentText = 'This is the comment I am posting';
   // My instagram user information
-  const myUsername = 'myEmail@gmail.com';
+  const myUsername = 'myUsername';
   const myPassword = 'myPassword123';
   // Number of times the page should refresh
   // Each iteration takes around 2.5 seconds
@@ -22,30 +22,30 @@ describe('Instagram fast commenter', function() {
   browser.driver.sleep(1000);
   browser.driver.findElement(by.css('[name="username"]')).sendKeys(myUsername);
   browser.driver.findElement(by.css('[name="password"]')).sendKeys(myPassword);
-  browser.driver.findElement(by.css('._5f5mN')).click();
+  browser.driver.findElement(by.css('._L3NKy')).click(); //This is the login button
   browser.driver.sleep(1000);
   browser.driver.get(targetURL);
   browser.driver.sleep(1000);
 
   // Main function that runs in loop
   function instaAutoCommenter(numberOfIterations){
-    it('should win me a ticket for that cool music festival', function() {
-
-      browser.driver.findElement(by.css('.g47SY')).getText().then( (data) => {
+    it('I dont know why this is here but okay', function() {
+          //The below element is for the num of followers target acct has
+      browser.driver.findElement(by.css('.g47SY')).getText().then( (data) => { 
         console.log(numberOfIterations);
         console.log(data);
-        var auxNumberOfPosts = data.replace('.','');
+        var auxNumberOfPosts = data.replace(',',''); //replaces the , in total posts number
         auxNumberOfPosts = parseInt(auxNumberOfPosts, 10);
         console.log(auxNumberOfPosts);
         if(auxNumberOfPosts != numberOfPostsOnTargetProfile){
           browser.driver.sleep(1500);
-          browser.driver.findElement(by.css('._9AhH0')).click();
+          browser.driver.findElement(by.css('._9AhH0')).click(); //Most recent post 
           browser.driver.sleep(1500);
-          browser.driver.findElement(by.css('.Ypffh')).click();
+          browser.driver.findElement(by.css('.Ypffh')).click(); //comment button
           browser.driver.sleep(200);
           browser.driver.findElement(by.css('.Ypffh')).sendKeys(commentText);
           browser.driver.findElement(by.css('.Ypffh')).sendKeys(protractor.Key.ENTER);
-          browser.driver.sleep(2000);
+          browser.driver.sleep(5000); //changes from 2000 to this
           numberOfPostsOnTargetProfile++;
         }
         browser.driver.sleep(2500);
